458bb06a01313555162ceb84c2c4df75ddd82768
Devon R <sansgorialis@gmail.com>
2025-02-01 22:23:03 +0900

    Automatically detect commands in the assembly

--- TASauriaPlugin/Commands/Registry.cs
+++ TASauriaPlugin/Commands/Registry.cs
@@ -1,15 +1,22 @@
 namespace ScarletCafe.TASauriaPlugin.Commands;
 
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public static class Registry {
-    public static readonly List<ICommand> commands = [
-        new Emulation.FrameCountCommand(),
-        new Emulation.PauseCommand(),
-        new Test.EchoCommand(),
-    ];
+    // Automatically scan for and instantiate all the command implementations
+    public static readonly List<ICommand> commands =
+        [.. AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(selector => selector.GetTypes())
+        .Where(predicate =>
+            typeof(ICommand).IsAssignableFrom(predicate)
+            && !predicate.IsAbstract
+        )
+        .Select(predicate => predicate.GetConstructor([]))
+        .Where(constructor => constructor != null)
+        .Select(constructor => (ICommand)constructor.Invoke([]))];
 
     public static JObject Resolve(string path, JObject input) {
         // Try to find a command that matches the path.
